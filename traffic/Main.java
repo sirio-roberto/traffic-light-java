fa434619f0c5439204ec636feef842d1dc188ee5
Sirio <sirioroberto1@hotmail.com>
2023-07-04 17:28:38 -0300

    Added simple menu

--- traffic/Main.java
+++ traffic/Main.java
@@ -2,6 +2,16 @@ package traffic;
 
 public class Main {
   public static void main(String[] args){
-    System.out.println("Hello World!");
+    System.out.println("Welcome to the traffic management system!");
+    showMenu();
+  }
+
+  private static void showMenu() {
+    System.out.println("""
+            Menu:
+            1. Add
+            2. Delete
+            3. System
+            0. Quit""");
   }
 }
